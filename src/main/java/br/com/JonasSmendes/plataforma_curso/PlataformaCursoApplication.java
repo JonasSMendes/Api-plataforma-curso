ec67d51eb4e058b227b01adcfdc24cbff3aa4924
jonas mendes <jonas.jsm2903@gmail.com>
2025-05-13 13:48:48 -0300

    feat: start project

---
+++ src/main/java/br/com/JonasSmendes/plataforma_curso/PlataformaCursoApplication.java
@@ -0,0 +1,13 @@
+package br.com.JonasSmendes.plataforma_curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaCursoApplication.class, args);
+	}
+
+}
